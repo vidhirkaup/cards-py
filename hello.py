2daf346fde6313e6875d7f716094d1862b69b836
vidhir <vk4web@gmail.com>
2021-01-01 17:11:54 -0500

    basic commands

---
+++ hello.py
@@ -0,0 +1,71 @@
+print('hello world from file')
+
+print('========================')
+print('start')
+x = 5
+if x < 10:
+    print('----> small')
+if x > 20:
+    print('----> big')
+
+print('finish')
+
+print('========================')
+
+y = 5
+while y > 0 :
+    print(y)
+    y = y - 1
+print('blast-off')
+
+print('========================')
+
+# name = input('Enter file:')
+# handle = open(name, 'r')
+
+# counts = dict()
+# for line in handle:
+#     words = line.split()
+#     for word in words:
+#         counts[word] = counts.get(word, 0) + 1
+
+# bigCount = None
+# bigWord = None
+# for word, count in counts.items():
+#     if bigCount is None or count > bigCount:
+#         bigWord = word
+#         bigCount = count
+
+# print(bigWord, bigCount)
+
+print('========================')
+
+# euroFloor = input('Enter Euro Floor: ')
+# usFloor = int(euroFloor)0 + 1
+# print('US Floor: ', usFloor)
+
+print('========================')
+
+x = 1
+if x > 2 :
+    print('big')
+else : 
+    print('small')
+
+print('========================')
+
+x = 42
+if x < 10 :
+    print('S')
+elif x < 20 :
+    print('M')
+elif x < 30 :
+    print('L')
+elif x < 40 :
+    print('XL')
+else :
+    print('XXL')
+
+print('========================')
+
+
