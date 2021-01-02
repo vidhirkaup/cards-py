46f19964ed3dadf33340f4aaecf4a1234c4dd581
vidhir <vk4web@gmail.com>
2021-01-01 19:25:03 -0500

    added new functions

--- functions.py
+++ functions.py
@@ -13,6 +13,7 @@ hello()
 
 # Prints input within parenthesis
 def format_input(instr):
+    print('-------------')
     print('[', instr, ']')
 
 
@@ -21,3 +22,18 @@ format_input(big)
 
 small = min('Hello World')
 format_input(small)
+
+
+# Print hello
+def greet(lang):
+    if lang == 'es':
+        format_input('Hola')
+    elif lang == 'fr':
+        format_input('Bonjour')
+    else:
+        format_input('Hello')
+
+
+greet('en')
+greet('es')
+greet('fr')
