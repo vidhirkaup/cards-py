407ec1ebc381e5d39445c544fd7dd1e5ce89bec1
Vidhir Kaup <vidhir.kaup@gmail.com>
2021-01-02 07:29:34 -0500

    added basic loops

--- loops.py
+++ loops.py
@@ -1,5 +1,5 @@
 print()
-print('>>> While loop: ')
+print('>>> Basic While loop: ')
 
 x = 5
 while x > 0:
@@ -12,11 +12,24 @@ print(x)
 print()
 print('>>> While loop with break and continue: ')
 
-while True:
-    line = input('> ')
-    if line == 'done':
-        break
-    if line == '#':
-        continue
-    print(line)
-print('DONE')
+# while True:
+#     line = input('> ')
+#     if line == 'done':
+#         break
+#     if line == '#':
+#         continue
+#     print(line)
+# print('DONE')
+
+print()
+print('>>> Basic For loop: ')
+
+for i in [5, 4, 3, 2, 1]:
+    print(i)
+print('Blast Off')
+print()
+
+friends = ['Adam', 'Bob', 'Charles']
+for friend in friends:
+    print('Hello ', friend)
+print('Done')
