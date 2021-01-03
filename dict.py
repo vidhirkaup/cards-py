f01fede17b09f5e616732d4f1ea3fd3b0335f6a7
Vidhir Kaup <vidhir.kaup@gmail.com>
2021-01-03 14:37:40 -0500

    added some text files

--- dict.py
+++ dict.py
@@ -23,3 +23,24 @@ for color in colors:
     colors_hist[color] = colors_hist.get(color, 0) + 1
 print(colors_hist)
 
+word_counter = dict()
+words = list()
+fhandle = open('random-lines.txt')
+for line in fhandle:
+    words = line.split()
+    if len(words) == 0:
+        continue
+    # print(words)
+    for word in words:
+        word_counter[word] = word_counter.get(word, 0) + 1
+print(word_counter)
+print()
+
+for key in word_counter:
+    print(key, word_counter[key])
+
+print()
+for k, v in word_counter.items():
+    print(k, v)
+
+
