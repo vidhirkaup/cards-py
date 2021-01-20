309b92f98b8df02ca6a60373d702d97a03f47b33
Vidhir Kaup <vidhir.kaup@gmail.com>
2021-01-19 22:17:27 -0500

    using remote apis

--- geojson.py
+++ geojson.py
@@ -1,13 +1,38 @@
-import urllib.request, urllib.parse, urllib.error
 import json
+import urllib.parse
+import urllib.request
 
-basepath = 'http://maps.googleapis.com/maps/api/geocode/json?'
+basepath = 'http://api.openweathermap.org/data/2.5/weather?'
 
 while True:
-    address = input('Enter location: ')
-    if len(address) < 1:
-        break
-    url = basepath + urllib.parse.urlencode({'address': address})
+    params = dict()
+    print()
+    print('Lets find the weather... Type x to go back home')
+
+    city = input('Enter city eg.(Brampton):')
+    if len(city) < 1:
+        print('OK - lets try with zip code...')
+        zipcode = input('Enter zip,country eg.(94040,us): ')
+        if len(zipcode) < 1:
+            continue
+        else:
+            if zipcode == 'x':
+                break
+            params['zip'] = zipcode
+    else:
+        if city == 'x':
+            break
+        params['q'] = city
+
+    appId = input('Enter key: ')
+    if len(appId) < 1:
+        continue
+    else:
+        if appId == 'x':
+            break
+        params['appid'] = appId
+
+    url = basepath + urllib.parse.urlencode(params)
     print('Retrieving', url)
 
     uh = urllib.request.urlopen(url)
@@ -19,9 +44,9 @@ while True:
     except:
         js = None
 
-    if not js or 'status' not in js or js['status'] != 'OK':
+    print(js['cod'])
+    if not js or 'cod' not in js or js['cod'] != 200:
         print('=== Failed to retrieve ===')
-        print(data)
         continue
 
-
+    print(js['name'], '[', js['coord']['lon'], ',', js['coord']['lat'], ']')
